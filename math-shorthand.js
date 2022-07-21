0697a39ef1f51dbe763e2d7991bdf6a08de60536
Bappy71 <19103076@iubat.edu>
2022-07-21 03:57:30 -0700

    first commit

---
+++ math-shorthand.js
@@ -0,0 +1,36 @@
+var price1 = 30;
+var price2 = 10;
+var sum = price1 + price2;
+var difference = price1 - price2;
+var multiplication = price1 * price2;
+var divition = price1 / price2;
+
+console.log(sum);
+console.log(difference);
+console.log(multiplication);
+console.log(divition);
+//
+
+var doublePrice = price2 * 10;
+var newPrice = price1 + 10;
+price1 += 10;
+
+//price2 = price2 - 2;
+price2 -= 2;
+console.log(price2)
+//console.log(price1);
+//console.log(newPrice);
+
+var age = 25;
+//age = age + 1;
+//age += 1;
+age++;
+console.log(age);
+
+var love = 100;
+//love = love - 1;
+love -= 1;
+love--;
+console.log(love);
+
+
