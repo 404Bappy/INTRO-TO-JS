19f9e35629075efb05efd64d77b85a270da933d1
Bappy71 <19103076@iubat.edu>
2022-07-21 03:27:21 -0700

    2nd commit

---
+++ mathemetical-operation.js
@@ -0,0 +1,30 @@
+var onionPrice = 40;
+var eggPrice = 30;
+var eggQuantity = 7;
+
+var moneyAvailable = 100;
+var ballPrice = 5;
+
+//Addition within 2 variable
+var totalPrice = (eggPrice + onionPrice);
+
+//substruction within 2 variable
+var difference = (onionPrice - eggPrice);
+
+//multiplication within 2 variable
+var eggPriceMultiplication = eggPrice * eggQuantity ;
+
+//divition within 2 variable
+var ballQuantity = moneyAvailable / ballPrice ;
+
+
+console.log(onionPrice);
+console.log(eggPrice);
+console.log(onionPrice + eggPrice);
+console.log(totalPrice);
+console.log(difference);
+console.log(eggPriceMultiplication);
+console.log(ballQuantity);
+
+
+
