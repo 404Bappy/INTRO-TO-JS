15cb79597cceaf8a51952dccbff09ef2a0845cf1
Bappy71 <19103076@iubat.edu>
2022-07-22 04:13:06 -0700

    first commit

---
+++ conversion.js
@@ -0,0 +1,36 @@
+var firstName = "arafat";
+var lastName = "BAppy";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+var ballPrice = "30";
+var eggPrice = "20";
+var total = ballPrice + eggPrice;
+console.log(total);
+
+//convert string to integer!!
+
+var ballPriceNumber = parseInt(ballPrice);
+
+//Integer Number
+var cat = 3;
+
+//Float Number
+var price = 30.33;
+
+
+var ballPrice = "30.99";
+var eggPrice = "20";
+var ballPriceNumber = parseInt(ballPrice);
+
+console.log(ballPrice);
+console.log(ballPriceNumber);
+
+//parseFolat(); keep 0.00
+
+var ballPriceNumber = parseFloat(ballPrice);
+
+console.log(ballPriceNumber);
+
+
