f23e390789dd104a512017a2f7c0efb617713dd7
Bappy71 <19103076@iubat.edu>
2022-09-08 23:55:34 +0600

    first commit

--- Variable.js
+++ Variable.js
@@ -14,3 +14,14 @@ var pass = true;
 var subscribed = false
 
 console.log(name);
+
+
+
+//Declare a variable named favoriteFood using the var keyword and assign to it the string 'pizza'.//
+var favoriteFood = 'pizza';
+
+//Declare a variable named numOfSlices using the var keyword and assign to it the number 8.
+var numOfSlices = 8;
+
+//Under the numOfSlices variable, use console.log() to print the value saved to favoriteFood.-->On the following line, use console.log() to print the value saved to numOfSlices.
+console.log(favoriteFood , numOfSlices);
