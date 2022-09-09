f62d918333adcfcfab8cd48af6ab95ed8e61bcdc
Bappy71 <19103076@iubat.edu>
2022-09-09 08:40:39 +0600

    first commit

--- mathemetical-operation.js
+++ mathemetical-operation.js
@@ -42,3 +42,33 @@ var remaining = mango % hungryMan;
 var deviding = mango / hungryMan;
 console.log(remaining);
 console.log(deviding);
+
+
+
+
+                                /////// Mathematical Assignment Operators //////
+
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+
+levelUp += 5;
+
+powerLevel -= 100;
+
+multiplyMe *= 11;
+
+quarterMe /=4;
+
+
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
