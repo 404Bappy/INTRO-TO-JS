84eda0b2f908355e876e98f6e480691d474e63f8
Bappy71 <19103076@iubat.edu>
2022-09-09 08:29:09 +0600

    first commit

--- Variable.js
+++ Variable.js
@@ -27,12 +27,35 @@ var numOfSlices = 8;
 console.log(favoriteFood , numOfSlices);
 
 
-//////Let Variable /////////
 
-//Create a let variable called changeMe and set it equal to the boolean true.//
+
+                                  //////Let Variable /////////
+
+/
+/Create a let variable called changeMe and set it equal to the boolean true.//
 let changeMe = true;
 
 /*On the line after changeMe is declared, set the value of changeMe to be the boolean false.
 To check if changeMe was reassigned, log the value saved to changeMe to the console.*/
 changeMe = false;
 console.log(changeMe);
+
+
+                                 /////// Const Variable ///////
+                            
+
+
+
+ /*Create a constant variable named entree and set it to equal to the string 'Enchiladas'.*/
+
+const entree = 'Enchiladas';
+
+/*Just to check that you’ve saved the value of 'Enchiladas' to entree, log the value of entree to the console.*/
+console.log(entree);
+
+/*Great, let’s see what happens if you try to reassign a constant variable.
+
+Paste the following code to the bottom of your program.This code throws the following error when you run your code:
+
+TypeError: Assignment to constant variable.*/
+entree = 'Tacos'
