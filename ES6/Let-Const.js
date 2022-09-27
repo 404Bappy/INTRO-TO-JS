ccf7b6c543e13cc19f3f6465525fcfb7e960d47d
Bappy71 <19103076@iubat.edu>
2022-09-28 03:22:23 +0600

    first commit

---
+++ ES6/Let-Const.js
@@ -0,0 +1,8 @@
+const balance = 122000;
+balance = 230; /// It will not Work Because const is nonChangeable///
+
+let Balance = 1300;
+Balance = 200; /// It will Work Because const is Changeable///
+
+
+var variable = 'rejected'; /// We are not using var anymore //
