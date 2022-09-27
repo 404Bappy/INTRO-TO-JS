bb7c3cc6f3f710d4bbba4064f7da39466368e660
Bappy71 <19103076@iubat.edu>
2022-09-27 20:29:49 +0600

    first commit

--- ES6/Basic.js
+++ ES6/Basic.js
@@ -17,3 +17,12 @@ if(deposit != 500){
 if(deposit >= 500){
 
 }
+
+                //Array//
+const number = [45, 54, 666, 777, 888, 999, 222];
+const numberCount = number.length;
+number.pop();
+number.push(666);
+number[3] =666;
+if(number.indexOf(999) != -1)
+
