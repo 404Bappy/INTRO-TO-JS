5c685aa588f19a11134134529767446d1a7f727e
Bappy71 <19103076@iubat.edu>
2022-09-28 02:12:25 +0600

    first commit

--- ES6/Dom/Dom.js
+++ ES6/Dom/Dom.js
@@ -3,4 +3,14 @@ document.getElementById('Add-Border').addEventListener('click',function(){
     container.style.border = '3px solid red';
 
 })
+
+document.getElementById('Add-Background').addEventListener('click',function(){
+    const friends = document.getElementsByClassName('friend');
+for(const friend of friends){
+    friend.style.backgroundColor = 'lightgreen';
+}
+
+})
+
+
  
