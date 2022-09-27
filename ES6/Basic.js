2e9f237ff99d4a9bb63eb79415db33e3962c80ba
Bappy71 <19103076@iubat.edu>
2022-09-27 20:25:30 +0600

    first commit

---
+++ ES6/Basic.js
@@ -0,0 +1,19 @@
+//Variable//
+var deposit =400;
+      
+             //Condition//
+if(deposit > 500){
+    
+}
+if(deposit > 500){
+
+}
+if(deposit == 500){
+
+}
+if(deposit != 500){
+
+}
+if(deposit >= 500){
+
+}
