470fcbf32b3def7c5d5eb39d6fe57f16eb42c210
Bappy71 <19103076@iubat.edu>
2022-09-28 02:03:34 +0600

    first commit

---
+++ ES6/Dom/Dom.js
@@ -0,0 +1,6 @@
+document.getElementById('Add-Border').addEventListener('click',function(){
+    const container = document.getElementById('friend-container');
+    container.style.border = '3px solid red';
+
+})
+ 
