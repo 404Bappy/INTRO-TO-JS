ae5abff5f7a934f5fad56e35095446163afc6474
Bappy71 <19103076@iubat.edu>
2022-09-29 20:28:46 +0600

    first commit

--- ES6/spread.js
+++ ES6/spread.js
@@ -5,3 +5,8 @@ const numbers = [20, 23, 25, 30, 90, 100];
 const max = math.max(23,34,77,99,1000,20000,900,1165)
 const maxInArray = math.max9(numbers);
 console.log(max,maxInArray);
+
+
+const numbers2 = [numbers , 88];
+console.log(numbers);
+console.log(numbers2);
