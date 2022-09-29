c6654e801a8dc807883826096b035fc62a61c847
Bappy71 <19103076@iubat.edu>
2022-09-29 08:24:56 +0600

    first commit

---
+++ ES6/template-string.js
@@ -0,0 +1,5 @@
+const bappy = 'he is a football player';
+const rabbi = "he is a good singer";
+const alim = `he is a good writter`
+
+const multiline = 'This is may working station'+'And im very comfortable with my place'
