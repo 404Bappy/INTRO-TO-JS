3b550a8de19eae983ca0dea98dde6ed4df0ce385
Bappy71 <19103076@iubat.edu>
2022-09-29 08:29:16 +0600

    first commit

--- ES6/template-string.js
+++ ES6/template-string.js
@@ -3,3 +3,11 @@ const rabbi = "he is a good singer";
 const alim = `he is a good writter`
 
 const multiline = 'This is may working station'+'And im very comfortable with my place'
+
+const multiLineNew = `this is multiline
+this is second line 
+this is 3rd line
+this is fourthline
+this is 5th line
+`
+console.log(multiLineNew);
