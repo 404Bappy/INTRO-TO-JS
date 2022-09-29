737ab8c39db605a81ab0bc1c98a3f41e487845b8
Bappy71 <19103076@iubat.edu>
2022-09-29 19:48:17 +0600

    first commit

---
+++ ES6/arrow-function.js
@@ -0,0 +1,13 @@
+//Function Declare
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+
+//document.getElementById('button').onclick = function
+const sum1 = add(15, 15);
+console.log(sum1);  
+
+const result = (num4, num5) => num4 + num5;
+
+console.log(result(5, 6));
