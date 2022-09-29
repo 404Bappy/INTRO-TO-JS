71cd6bb3077cf855772e447ff9cd32b4b5e21cab
Bappy71 <19103076@iubat.edu>
2022-09-29 20:01:34 +0600

    first commit

--- ES6/arrow-function.js
+++ ES6/arrow-function.js
@@ -8,8 +8,7 @@ const add2 = function add2(num1, num2){
     return num1 + num2;
 }
 //function expresion anonimous //
-const add3 = function (num1, num2){|
-
+const add3 = function (num1, num2){
 }
 const sum1 = add1(15, 17);
 const sum2 = add2(15, 17);
