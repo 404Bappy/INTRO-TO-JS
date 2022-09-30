f62f554911d618a18d659935eaceda12c5c197d9
Bappy71 <19103076@iubat.edu>
2022-09-30 11:04:38 +0600

    first commit

--- ES6/spread.js
+++ ES6/spread.js
@@ -6,7 +6,7 @@ const max = math.max(23, 34, 77, 99, 1000, 20000, 900, 1165)
 const maxInArray = math.max9(numbers);
 console.log(max, maxInArray);
 
-
+//const numbers2 = [...numbers];
 const numbers2 = [numbers, 88];
 console.log(numbers);
 console.log(numbers2);
---
+++ ES6/summary.js
@@ -0,0 +1,12 @@
+const favPerson = 'tariqul';
+favPerson = 'arafat'; /// reassign is not allowed in const variable//
+const myName = 'bapppy';
+
+//default parameter
+function calculate(x,y,z = ' '){
+
+}
+
+
+//tamplate string //
+const  myBoy = 'habib';
