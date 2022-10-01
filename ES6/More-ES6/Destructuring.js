97a1970beb39a8495682f3dd92dd648fe95a3179
Bappy71 <19103076@iubat.edu>
2022-10-01 06:41:57 +0600

    first commit

--- ES6/More-ES6/Destructuring.js
+++ ES6/More-ES6/Destructuring.js
@@ -1,2 +1,13 @@
-const fish = { id: 50, name: 'King Hilsha', Price: 9000, phone: '01849675876', address: 'Chandpur', color: 'siler' };
-console.log(fish.phone);
+const fish = { id: 50, name: 'King Hilsha', price: 9000, phone: '01849675876', address: 'Chandpur', color: 'siler' };
+
+const phone = fish.phone;
+const price = fish.price;
+const color = fish.color;
+const id =fish.id;
+
+console.log(phone ,price);
+console.log(phone,color);
+console.log(phone);
+console.log(phone);
+console.log(phone);
+console.log(phone);
