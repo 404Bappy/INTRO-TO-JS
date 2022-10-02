65b58f22d8b6667d9587f38d5412d7383fbc5157
Bappy71 <19103076@iubat.edu>
2022-10-02 22:55:53 +0600

    first commit

--- ES6/More-ES6/Chaining.js
+++ ES6/More-ES6/Chaining.js
@@ -1,2 +1,10 @@
 //declear variable based on the name of an object property
 const myObject = {x:2, y:50, z:400, a:23, b:68};
+
+const {x, b} = myObject;
+console.log(b);
+
+/// Destructing array//
+
+const [p, q] = [23, 40, 90, 80, 70];
+console.log(p,q);
