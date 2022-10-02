841a47ebad24af35e80346ab1248a557db6a373d
Bappy71 <19103076@iubat.edu>
2022-10-02 23:11:07 +0600

    first commit

--- ES6/More-ES6/map.js
+++ ES6/More-ES6/map.js
@@ -1,5 +1,13 @@
-const numbers = [3, 5, 6,8, 9, 10, 13];
+const number = [3, 5, 6,8, 9, 10, 13];
 const output = [];
+
+function doubleOld(number){
+    return number * 2;
+}
+    
+
+const doubleIt =number => number *2;
+
 for(const number of numbers){
     const result = number*2;
     output.push(result);
